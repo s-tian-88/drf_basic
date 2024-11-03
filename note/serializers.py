a09b1aac6f46416cfe23df885fc0644799cf0f82
S.Tian <s.tian.spb@gmail.com>
2024-11-03 21:43:42 +0300

    review basic serializator work

--- note/serializers.py
+++ note/serializers.py
@@ -1,9 +1,29 @@
+import io
+from django.core.serializers import serialize
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
-from note.models import Note
+class NoteModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
 
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Note
-        fields = '__all__'
+class NoteSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
 
+def encode():
+    model = NoteModel('note title', 'note content')
+    model_sr = NoteSerializer(model)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
+
+def decode():
+    stream = io.BytesIO(b'{"title":"note title","content":"note content"}')
+    data = JSONParser().parse(stream)
+    serialize = NoteSerializer(data=data)
+    serialize.is_valid()
+    print(serialize.validated_data)
+    
--- note/views.py
+++ note/views.py
@@ -1,8 +1,4 @@
-from rest_framework import generics # набор базовых классов для представления
-
 from .models import Note
 from .serializers import NoteSerializer
 
-class NoteAPIView(generics.ListAPIView):
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
+
